bc05921389e15c1c79523297bc683e40647e03d2
Saikumar Bandigi <bandigisaikumar@gmail.com>
2026-01-05 23:37:55 +0530

    5th Jan 2nd commit

--- src/leetcode/NumberTheoryTut/CountPrimes.java
+++ src/leetcode/NumberTheoryTut/CountPrimes.java
@@ -3,9 +3,7 @@ package leetcode.NumberTheoryTut;
 public class CountPrimes {
     public static void main(String[] args) {
         int tillLessThanHowMuch = 5;
-
         int count = 0;
-
         int num = 2;
         while (num < tillLessThanHowMuch) {
             if (isPrime(num)) {
@@ -14,6 +12,31 @@ public class CountPrimes {
             num++;
         }
         System.out.println(count);
+        System.out.println(countPrimes(tillLessThanHowMuch));
+    }
+    public static int countPrimes(int n) {
+        if (n <= 2)
+            return 0;
+        boolean[] isPrime = new boolean[n];
+        for (int i = 2; i < n; i++) {
+            isPrime[i] = true;
+        }
+
+        for (int i = 2; i * i < n; i++) {
+            if (isPrime[i]) {
+                for (int j = i * i; j < n; j += i) {
+                    isPrime[j] = false;
+                }
+            }
+        }
+
+        int count = 0;
+        for (int i = 2; i < n; i++) {
+            if (isPrime[i])
+                count++;
+        }
+
+        return count;
     }
 
     static boolean isPrime(int num) {
@@ -24,7 +47,7 @@ public class CountPrimes {
             return true;
         }
         if (num % 2 == 0) {
-            return false
+            return false;
         }
         for (int i = 3; i * i <= num; i++) {
             if (num % i == 0) {
