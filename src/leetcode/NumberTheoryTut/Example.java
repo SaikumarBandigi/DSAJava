bae3ee7b87eb9527c713bc78684ca2e5054d6cdc
Saikumar Bandigi <bandigisaikumar@gmail.com>
2026-01-06 21:24:44 +0530

    Jan 6th third commit

--- src/leetcode/NumberTheoryTut/Example.java
+++ src/leetcode/NumberTheoryTut/Example.java
@@ -2,18 +2,22 @@ package leetcode.NumberTheoryTut;
 
 public class Example {
     public static void main(String[] args) {
-        int num = 123;
+        int num = 66;
         System.out.println(addAllDIGITS(num));
         System.out.println(reverseANumber(num));
     }
 
     static int addAllDIGITS(int num) {
-        int sum = 0;
-        while (num > 0) {
-            sum += num % 10;
-            num /= 10;
+        while (num >= 10) {
+            int sum = 0;
+            while (num > 0) {
+                int lastDigit = num % 10;
+                sum += lastDigit;
+                num /= 10;
+            }
+            num = sum;
         }
-        return sum;
+        return num;
     }
 
     static int reverseANumber(int num) {
