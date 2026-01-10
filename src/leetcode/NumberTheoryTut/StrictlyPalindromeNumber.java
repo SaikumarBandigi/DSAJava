483a485a93aeb63a60683f5d4e0acfe99faacd8b
Saikumar Bandigi <bandigisaikumar@gmail.com>
2026-01-10 19:30:21 +0530

    Jan 10th 3rd commit

---
+++ src/leetcode/NumberTheoryTut/StrictlyPalindromeNumber.java
@@ -0,0 +1,10 @@
+package leetcode.NumberTheoryTut;
+
+public class StrictlyPalindromeNumber {
+    public static void main(String[] args) {
+
+    }
+    public boolean isStrictlyPalindromic(int n) {
+        return false;
+    }
+}
