a9de54b2aec85dd3d3b901d02d771d33c08d585a
Saikumar Bandigi <bandigisaikumar@gmail.com>
2026-01-11 09:26:21 +0530

    Jan 11th second commit

--- src/leetcode/arrayconcept/ContainsDuplicate.java
+++ src/leetcode/arrayconcept/ContainsDuplicate.java
@@ -7,6 +7,7 @@ public class ContainsDuplicate {
     public static void main(String[] args) {
         int[] arr = {1, 2, 3, 1};
         System.out.println(containsDupli(arr));
+        System.out.println(containsDupliBruteForce(arr));
     }
 
     static boolean containsDupli(int[] arr) {
@@ -17,6 +18,17 @@ public class ContainsDuplicate {
                 return true;
             }
             set.add(num);
+        }
+        return false;
+    }
+
+    static boolean containsDupliBruteForce(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] == nums[j]) {
+                    return true;
+                }
+            }
         }
         return false;
     }
