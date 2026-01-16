a8490cf18ea955608569306ae7868b7eebeae0cc
Saikumar Bandigi <bandigisaikumar@gmail.com>
2026-01-16 10:43:44 +0530

    Jan 16th Second Commit

--- src/patternMust/slidingwindow/MinimumSizeSubArraySum.java
+++ src/patternMust/slidingwindow/MinimumSizeSubArraySum.java
@@ -6,6 +6,7 @@ public class MinimumSizeSubArraySum {
         int target = 7;
 
         System.out.println(minSubArrayLenBruteForce(target, arr));
+        System.out.println(minSubArrayLenOptimized(target, arr));
     }
 
     public static int minSubArrayLenBruteForce(int target, int[] nums) {
@@ -14,14 +15,14 @@ public class MinimumSizeSubArraySum {
         int minLen = Integer.MAX_VALUE;
 
         // start index
-        for (int i = 0; i < n; i++) {
+        for (int left = 0; left < n; left++) {
             int sum = 0;
             // end index
-            for (int j = i; j < n; j++) {
-                sum += nums[j];
+            for (int right = left; right < n; right++) {
+                sum += nums[right];
 
                 if (sum >= target) {
-                    minLen = Math.min(minLen, j - i + 1);
+                    minLen = Math.min(minLen, right - left + 1);
                     break; // no need to extend further
                 }
             }
@@ -29,4 +30,25 @@ public class MinimumSizeSubArraySum {
         return minLen == Integer.MAX_VALUE ? 0 : minLen;
 
     }
+
+    public static int minSubArrayLenOptimized(int target, int[] nums) {
+
+        int left = 0;
+        int sum = 0;
+        int minLen = Integer.MAX_VALUE;
+
+        for (int right = 0; right < nums.length; right++) {
+            sum += nums[right];
+
+            // shrink window while condition satisfied
+            while (sum >= target) {
+                minLen = Math.min(minLen, right - left + 1);
+                sum -= nums[left];
+                left++;
+            }
+        }
+
+        return minLen == Integer.MAX_VALUE ? 0 : minLen;
+    }
+
 }
