84451ca3634a1c3c21c4071a37782bf58c4e7fb1
Saikumar Bandigi <bandigisaikumar@gmail.com>
2026-01-17 11:13:29 +0530

    Jan 17th Second Commit

--- src/leetcode/gametheory/Nimgame.java
+++ src/leetcode/gametheory/Nimgame.java
@@ -2,8 +2,9 @@ package leetcode.gametheory;
 
 public class Nimgame {
     public static void main(String[] args) {
-        System.out.println(new Nimgame().canWinNim(4));
+        System.out.println(new Nimgame().canWinNim(5));
     }
+
     public boolean canWinNim(int n) {
         return n % 4 != 0;
     }
--- src/patternMust/slidingwindow/MaximumSubarrayBruteForce.java
+++ src/patternMust/slidingwindow/MaximumSubarrayBruteForce.java
@@ -2,8 +2,9 @@ package patternMust.slidingwindow;
 
 public class MaximumSubarrayBruteForce {
     public static void main(String[] args) {
-        int[] nums = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+        int[] nums = {1, 2, 3, 4};
         System.out.println(maxSubArray(nums));
+        System.out.println(maxSumArrayOptimal(nums));
     }
 
     public static int maxSubArray(int[] nums) {
@@ -16,6 +17,18 @@ public class MaximumSubarrayBruteForce {
                 maxSum = Math.max(maxSum, currentSum);
             }
         }
+        return maxSum;
+    }
+
+    public static int maxSumArrayOptimal(int[] nums) {
+        int currentSum = nums[0];
+        int maxSum = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+            currentSum = Math.max(nums[i], currentSum + nums[i]);
+            maxSum = Math.max(maxSum, currentSum);
+        }
+
         return maxSum;
     }
 
