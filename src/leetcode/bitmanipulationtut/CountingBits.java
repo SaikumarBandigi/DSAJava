ae8ce9ebddecf5845461d456853645649bf48eed
Saikumar Bandigi <bandigisaikumar@gmail.com>
2026-01-24 19:44:02 +0530

    Jan 24th Fourth Commit

--- src/leetcode/bitmanipulationtut/CountingBits.java
+++ src/leetcode/bitmanipulationtut/CountingBits.java
@@ -1,12 +1,48 @@
 package leetcode.bitmanipulationtut;
 
+import java.util.Arrays;
+
 public class CountingBits {
     public static void main(String[] args) {
 
-        System.out.println(isSetBit(2, 1));
+        System.out.println(Arrays.toString(countBits(5)));
     }
 
-    private static boolean isSetBit(int num, int bitPosition) {
+    static int[] countBitsOptimized(int num) {
+        int[] arr = new int[num + 1];
+
+        for (int i = 1; i <= num; i++) {
+            arr[i] = arr[i >> 1] + (i & 1);
+        }
+
+        return arr;
+    }
+
+    static int[] countBits(int num) {
+        int[] arr = new int[num + 1];
+        arr[0] = 0;
+
+        for (int i = 1; i <= num; i++) {
+            arr[i] = CountSetBitsOfAParticularNumber(i);
+        }
+
+        return arr;
+    }
+
+    static int CountSetBitsOfAParticularNumber(int num) {
+
+        int counter = 0;
+
+        for (int bitPosition = 1; bitPosition <= 32; bitPosition++) {
+            if (isSetBit(num, bitPosition)) {
+                counter++;
+            }
+        }
+        return counter;
+    }
+
+    static boolean isSetBit(int num, int bitPosition) {
         return ((num >> (bitPosition - 1)) & 1) == 1;
     }
+
 }
