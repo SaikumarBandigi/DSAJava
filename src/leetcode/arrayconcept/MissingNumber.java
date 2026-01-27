79565293c98ea4750626b1f4285d2a77c1f3ad12
Saikumar Bandigi <bandigisaikumar@gmail.com>
2026-01-27 23:22:19 +0530

    Jan 27th First Commit

---
+++ src/leetcode/arrayconcept/MissingNumber.java
@@ -0,0 +1,21 @@
+package leetcode.arrayconcept;
+
+public class MissingNumber {
+    public static void main(String[] args) {
+        int[] nums = {9, 6, 4, 2, 3, 5, 7, 0, 1};
+        System.out.println(missingNumber(nums));
+    }
+
+    static int missingNumber(int[] nums) {
+        int n = nums.length;
+        int total = n * (n + 1) / 2;
+
+        int sum = 0;
+
+        for (int num : nums) {
+            sum += num;
+        }
+        return total - sum;
+    }
+
+}
