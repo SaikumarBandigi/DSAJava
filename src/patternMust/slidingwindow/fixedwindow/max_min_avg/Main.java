3895b579693f23635aabc3c02e360d15281c6a6d
Saikumar Bandigi <bandigisaikumar@gmail.com>
2026-01-28 11:04:43 +0530

    Jan 28th First Commit

---
+++ src/patternMust/slidingwindow/fixedwindow/max_min_avg/Main.java
@@ -0,0 +1,67 @@
+package patternMust.slidingwindow.fixedwindow.max_min_avg;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int[] arr = {2, 1, 5, 1, 3, 2};
+        int k = 3;
+
+        System.out.println(MaxSumSubarrayBrute(arr, k));
+        System.out.println(MaxSumSubarraySlidingWindow(arr, k));
+        System.out.println(MaxSumFirstWindowThenSlide(arr, k));
+    }
+
+    public static int MaxSumSubarrayBrute(int[] arr, int k) {
+        int maxSum = Integer.MIN_VALUE;
+
+        for (int i = 0; i <= arr.length - k; i++) {
+            int sum = 0;
+            for (int j = i; j < i + k; j++) {
+                sum += arr[j];
+            }
+            maxSum = Math.max(maxSum, sum);
+        }
+        return maxSum;
+    }
+
+    public static int MaxSumSubarraySlidingWindow(int[] arr, int k) {
+        int left = 0;
+        int windowSum = 0;
+        int maxSum = Integer.MIN_VALUE;
+
+        // expand window using right pointer
+        for (int right = 0; right < arr.length; right++) {
+            windowSum += arr[right];   // add element
+            // when window size == k
+            if (right - left + 1 == k) {
+                maxSum = Math.max(maxSum, windowSum);
+                windowSum -= arr[left]; // remove element
+                left++;                 // slide window
+            }
+        }
+
+        return maxSum;
+    }
+
+    public static int MaxSumFirstWindowThenSlide(int[] arr, int k) {
+        int windowSum = 0;
+
+        // 🔹 1) FIRST WINDOW
+        for (int i = 0; i < k; i++) {
+            windowSum += arr[i];
+        }
+
+        int maxSum = windowSum;
+
+        // 🔹 2) SLIDE WINDOW TO THE RIGHT
+        for (int i = k; i < arr.length; i++) {
+            windowSum = windowSum + arr[i] - arr[i - k];
+            maxSum = Math.max(maxSum, windowSum);
+        }
+
+        return maxSum;
+    }
+
+
+}
+
