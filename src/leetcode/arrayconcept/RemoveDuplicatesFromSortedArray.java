6c5e903b3df26ac16c0395c28efbd51ecddd5e66
Saikumar Bandigi <bandigisaikumar@gmail.com>
2026-02-07 21:27:27 +0530

    updated RemoveDuplicatesFromSortedArray file

--- src/leetcode/arrayconcept/RemoveDuplicatesFromSortedArray.java
+++ src/leetcode/arrayconcept/RemoveDuplicatesFromSortedArray.java
@@ -22,7 +22,8 @@ public class RemoveDuplicatesFromSortedArray {
     }
 
     public static void main(String[] args) {
-
+        int[] nums = {0, 0, 1, 1, 1, 2, 2, 3, 3, 4};
+        System.out.println(new RemoveDuplicatesFromSortedArray().removeDuplicates(nums));
     }
 
 }
