24200efc2587e353af1767dc1d2e6519f819a496
Saikumar Bandigi <bandigisaikumar@gmail.com>
2026-02-12 13:17:27 +0530

    updated reshapethematrix file

--- src/leetcode/matrixTut/ReshapeTheMatrix.java
+++ src/leetcode/matrixTut/ReshapeTheMatrix.java
@@ -8,9 +8,9 @@ public class ReshapeTheMatrix {
         int c = 2;
         int[][] res = matrixReshape(mat, r, c);
 
-        for (int[] re : res) {
-            for (int i : re) {
-                System.out.print(i + " ");
+        for (int[] eachRow : res) {
+            for (int elementInRow : eachRow) {
+                System.out.print(elementInRow + " ");
             }
             System.out.println();
         }
@@ -22,18 +22,17 @@ public class ReshapeTheMatrix {
         int m = mat.length;
         int n = mat[0].length;
 
-        // Check if reshape is possible
         if (m * n != r * c) {
             return mat;
         }
 
-        int[][] result = new int[r][c];
+        int[][] res = new int[r][c];
 
         for (int i = 0; i < m * n; i++) {
-            result[i / c][i % c] = mat[i / n][i % n];
+            res[i / c][i % c] = mat[i / n][i % n];
         }
+        return res;
 
-        return result;
     }
 
 }
