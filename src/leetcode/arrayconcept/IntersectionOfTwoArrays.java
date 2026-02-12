2076c8e7ad8ab9349a413743b66371ad15dcf40b
Saikumar Bandigi <bandigisaikumar@gmail.com>
2026-02-12 13:35:14 +0530

    added main method with input values in IntersectionOfTwoArrays file

--- src/leetcode/arrayconcept/IntersectionOfTwoArrays.java
+++ src/leetcode/arrayconcept/IntersectionOfTwoArrays.java
@@ -1,11 +1,18 @@
 package leetcode.arrayconcept;
 
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
 public class IntersectionOfTwoArrays {
+    public static void main(String[] args) {
+        int[] nums1 = {1, 2, 2, 1};
+        int[] nums2 = {1, 2};
 
-    public int[] intersection(int[] nums1, int[] nums2) {
+        System.out.println(Arrays.toString(intersection(nums1, nums2)));
+    }
+
+    public static int[] intersection(int[] nums1, int[] nums2) {
         Set<Integer> set1 = new HashSet<>();
         Set<Integer> result = new HashSet<>();
 
