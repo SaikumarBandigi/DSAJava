55e5b6ae76abe5ed4f79f6a6caf066e7d08e05bd
Saikumar Bandigi <bandigisaikumar@gmail.com>
2026-02-15 14:38:00 +0530

    added main method in NumberOf1Bits

--- src/leetcode/bitmanipulationtut/NumberOf1Bits.java
+++ src/leetcode/bitmanipulationtut/NumberOf1Bits.java
@@ -1,14 +1,20 @@
+package leetcode.bitmanipulationtut;
+
 public class NumberOf1Bits {
-    public int hammingWeight(int n) {
+    public static void main(String[] args) {
+        System.out.println(hammingWeight(5));
+    }
+
+    public static int hammingWeight(int n) {
         int count = 0;
-        
+
         for (int i = 0; i < 32; i++) {
             if ((n & 1) == 1) {
                 count++;
             }
             n >>>= 1;
         }
-        
         return count;
     }
+
 }
