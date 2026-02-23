ad445773a9d2f2389ec3d245cd562786e59079b4
Saikumar Bandigi <bandigisaikumar@gmail.com>
2026-02-23 14:42:21 +0530

    added MaximumDepthOfBinaryTree

---
+++ src/leetcode/treeConcept/MaximumDepthOfBinaryTree.java
@@ -0,0 +1,60 @@
+package leetcode.treeConcept;
+
+public class MaximumDepthOfBinaryTree {
+
+    public static void main(String[] args) {
+
+        /*
+                1
+               / \
+              2   3
+             /
+            4
+
+
+        */
+
+        TreeNode root = new TreeNode(1);
+
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+
+        root.left.left = new TreeNode(4);
+
+
+        int depth = maxDepth(root);
+
+        System.out.println("Maximum Depth: " + depth);
+    }
+
+    public static int maxDepth(TreeNode root) {
+
+        if (root == null) {
+            return 0;
+        }
+
+        int left = maxDepth(root.left);
+        int right = maxDepth(root.right);
+
+        return 1 + Math.max(left, right);
+    }
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
