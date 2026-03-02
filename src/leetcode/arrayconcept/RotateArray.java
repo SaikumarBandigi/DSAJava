0d91f4c423a0048a849cf63b32498ce1575bfd9d
Saikumar Bandigi <bandigisaikumar@gmail.com>
2026-03-02 11:42:03 +0530

    added RotateArray file

---
+++ src/leetcode/arrayconcept/RotateArray.java
@@ -0,0 +1,35 @@
+package leetcode.arrayconcept;
+
+
+import java.util.Arrays;
+
+public class RotateArray {
+    public static void main(String[] args) {
+
+        int[] nums = {1, 2, 3, 4, 5,};
+        int k = 2;
+        rotate(nums, k);
+
+        System.out.println(Arrays.toString(nums));
+
+    }
+
+    public static void rotate(int[] nums, int k) {
+        int n = nums.length;
+        k = k % n;
+        reverse(nums, 0, n - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, n - 1);
+    }
+
+    static void reverse(int[] arr, int start, int end) {
+        while (start < end) {
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+    }
+
+}
