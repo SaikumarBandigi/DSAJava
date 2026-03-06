b1b1bb940c687d851bf3dd9b6a5a1be2b10556e6
Saikumar Bandigi <88974882+SaikumarBandigi@users.noreply.github.com>
2026-03-06 22:45:40 +0530

    Implement Longest Harmonious Subsequence algorithm

---
+++ src/leetcode/HashMapTut/LongestHarmoniousSubsequence.java
@@ -0,0 +1,22 @@
+class LongestHarmoniousSubsequence {
+
+    public int findLHS(int[] nums) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int max = 0;
+
+        for (int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+
+            if (map.containsKey(num + 1)) {
+                max = Math.max(max, map.get(num) + map.get(num + 1));
+            }
+
+            if (map.containsKey(num - 1)) {
+                max = Math.max(max, map.get(num) + map.get(num - 1));
+            }
+        }
+
+        return max;
+    }
+
+}
