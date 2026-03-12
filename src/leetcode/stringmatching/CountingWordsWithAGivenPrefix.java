1e753e2ed62715444e3882b018a459340c020884
Saikumar Bandigi <bandigisaikumar@gmail.com>
2026-03-12 19:10:10 +0530

    aded CountingWordsWithAGivenPrefix File

---
+++ src/leetcode/stringmatching/CountingWordsWithAGivenPrefix.java
@@ -0,0 +1,25 @@
+package leetcode.stringmatching;
+
+public class CountingWordsWithAGivenPrefix {
+
+    public static void main(String[] args) {
+
+        String[] words = {"pay", "attention", "practice", "attend"};
+        String pref = "at";
+        System.out.println(new CountingWordsWithAGivenPrefix().prefixCount(words, pref));
+
+    }
+
+    public int prefixCount(String[] words, String pref) {
+
+        int count = 0;
+
+        for (String word : words) {
+            if (word.startsWith(pref)) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+}
