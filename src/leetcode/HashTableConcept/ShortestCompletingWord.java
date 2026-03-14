54f306e89b592f01f6b684d5ae8efd7d6d487f29
Saikumar Bandigi <bandigisaikumar@gmail.com>
2026-03-14 13:22:04 +0530

    updated & added HashTable code

--- src/leetcode/HashTableConcept/ShortestCompletingWord.java
+++ src/leetcode/HashTableConcept/ShortestCompletingWord.java
@@ -1,17 +1,23 @@
 package leetcode.HashTableConcept;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class ShortestCompletingWord {
     public static void main(String[] args) {
 
         String licensePlate = "1s3 PSt";  // sspt
-        String[] words = {"steps", "sspt"};
+        String[] words = {"steps", "sspt"};  // another word spt
 
         System.out.println(new ShortestCompletingWord().shortestCompletingWord(licensePlate, words));
+        System.out.println(
+                new ShortestCompletingWord().shortestCompletingWordHashMap("1s3 PSt", new String[]{"sspt"}));
     }
+
 
     public String shortestCompletingWord(String licensePlate, String[] words) {
 
-        int[] licenseCount = new int[26];
+        int[] licenseCount = new int[26];  // sspt
 
         // Count letters in license plate
         for (char c : licensePlate.toCharArray()) {
@@ -45,6 +51,52 @@ public class ShortestCompletingWord {
                 return false;
             }
         }
+        return true;
+    }
+
+    public String shortestCompletingWordHashMap(String licensePlate, String[] words) {
+
+        HashMap<Character, Integer> licenseMap = new HashMap<>();  // s-2 p-1 t-1
+
+        // Build frequency map for license plate
+        for (char c : licensePlate.toLowerCase().toCharArray()) {
+            if (Character.isLetter(c)) {
+                licenseMap.put(c, licenseMap.getOrDefault(c, 0) + 1);
+            }
+        }
+
+        String result = null;
+
+        for (String word : words) {
+            HashMap<Character, Integer> wordMap = new HashMap<>();  // s-2 p-1 t-1
+
+            for (char c : word.toCharArray()) {
+                wordMap.put(c, wordMap.getOrDefault(c, 0) + 1);
+            }
+
+            if (isValidUsingtHashMap(licenseMap, wordMap)) {
+                if (result == null || word.length() < result.length()) {
+                    result = word;
+                }
+            }
+        }
+
+        return result;
+    }
+
+    private boolean isValidUsingtHashMap(HashMap<Character, Integer> licenseMap, HashMap<Character, Integer> wordMap) {
+
+        for (Map.Entry<Character, Integer> entry : licenseMap.entrySet()) {
+            char Lkey = entry.getKey();
+            int Lvalue = entry.getValue();
+
+            int Wvalue = wordMap.get(Lkey);
+
+            if (!wordMap.containsKey(Lkey) || Wvalue < Lvalue) {
+                return false;
+            }
+        }
+
         return true;
     }
 
