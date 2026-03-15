99f221b871ddf81ec71132eece2026c00b6365e0
Saikumar Bandigi <bandigisaikumar@gmail.com>
2026-03-15 09:46:34 +0530

    added four93 pack

--- src/leetcodecontest/weekly/Four93/CountCommasInRange.java
+++ src/leetcodecontest/weekly/Four93/CountCommasInRange.java
@@ -1,11 +1,12 @@
 package leetcodecontest.weekly.Four93;
 
+import java.text.NumberFormat;
+
 public class CountCommasInRange {
     public static void main(String[] args) {
 
-        int n = 1004590; // 10 pow 5
+        int n = 100000; // 10 pow 5
         System.out.println(new CountCommasInRange().countCommas(n));
-
     }
 
     int countCommas(int num) {
@@ -14,5 +15,6 @@ public class CountCommasInRange {
         }
         return num - 999;
     }
+
 
 }
