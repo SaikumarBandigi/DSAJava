193a340653c85d2df5327a893d210fcf48322edf
Saikumar Bandigi <bandigisaikumar@gmail.com>
2026-03-15 14:41:09 +0530

    added RearrangeCharactersToMakeTargetString File

---
+++ src/leetcode/countingTut/RearrangeCharactersToMakeTargetString.java
@@ -0,0 +1,38 @@
+package leetcode.countingTut;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class RearrangeCharactersToMakeTargetString {
+    public static void main(String[] args) {
+
+        String s = "saiias", target = "sai";
+        System.out.println(new RearrangeCharactersToMakeTargetString().rearrangeCharacters(s, target));
+
+    }
+
+    public int rearrangeCharacters(String s, String target) {
+
+        Map<Character, Integer> sMap = new HashMap<>();
+        Map<Character, Integer> tMap = new HashMap<>();
+
+        for (char ch : s.toCharArray()) {
+            sMap.put(ch, sMap.getOrDefault(ch, 0) + 1);
+        }
+
+        for (char ch : target.toCharArray()) {
+            tMap.put(ch, tMap.getOrDefault(ch, 0) + 1);
+        }
+
+        int result = Integer.MAX_VALUE;
+
+        for (char ch : tMap.keySet()) {
+            int countInS = sMap.getOrDefault(ch, 0);
+            int countInTarget = tMap.get(ch);
+            result = Math.min(result, countInS / countInTarget);
+        }
+
+        return result;
+    }
+
+}
