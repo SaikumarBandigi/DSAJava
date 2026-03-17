34633c167a3eb53f68f049cbe6a558f9dc7e2066
Saikumar Bandigi <bandigisaikumar@gmail.com>
2026-03-17 21:28:03 +0530

    added CheckifBinaryStringHasatMostOneSegmentofOnes File

--- src/leetcode/bitmanipulationtut/BitwiseANDOfNumbersRange.java
+++ src/leetcode/bitmanipulationtut/BitwiseANDOfNumbersRange.java
@@ -3,13 +3,12 @@ package leetcode.bitmanipulationtut;
 public class BitwiseANDOfNumbersRange {
     public static void main(String[] args) {
         int left = 5;
-        int right = 7;
+        int right = 8;
         //  System.out.println(new BitwiseANDOfNumbersRange().rangeBitwiseAndTLE(left, right));
         System.out.println(new BitwiseANDOfNumbersRange().rangeBitwiseAndUsingRemovingRightMostSetBit(left, right));
     }
 
     public int rangeBitwiseAndTLE(int left, int right) {
-
         int res = left;
         for (int i = left + 1; i <= right; i++) {
             res = bitwiseAND(res, i);
@@ -18,11 +17,9 @@ public class BitwiseANDOfNumbersRange {
     }
 
     public int rangeBitwiseAndUsingRemovingRightMostSetBit(int left, int right) {
-
         while (right > left) {
             right = right & (right - 1);
         }
-
         return right;
     }
 
