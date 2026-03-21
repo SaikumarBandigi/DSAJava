04d7b0c8787412d1e86262233460672446718e9b
Saikumar Bandigi <bandigisaikumar@gmail.com>
2026-03-21 13:31:47 +0530

    added CountTotalSetBits File

--- src/interviewBit/TrailingZeroes.java
+++ src/interviewBit/TrailingZeroes.java
@@ -9,11 +9,8 @@ public class TrailingZeroes {
     }
 
     public int solve(int A) {
-
         String ABinaryString = Integer.toBinaryString(A); // 10010
-
         int count = 0;
-
         for (int i = ABinaryString.length() - 1; i >= 0; i--) {
             if (ABinaryString.charAt(i) - '0' == 0) {
                 count++;
