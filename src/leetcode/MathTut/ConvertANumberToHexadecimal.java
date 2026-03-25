61e35bcf1e23719aeca76a7c2460a06c0159d93d
Saikumar Bandigi <bandigisaikumar@gmail.com>
2026-03-25 15:44:02 +0530

    added ConvertANumberToHexadecimal File

---
+++ src/leetcode/MathTut/ConvertANumberToHexadecimal.java
@@ -0,0 +1,26 @@
+package leetcode.MathTut;
+
+public class ConvertANumberToHexadecimal {
+
+    public static void main(String[] args) {
+
+    }
+
+    public String toHex(int num) {
+
+        if (num == 0) return "0";
+
+        char[] hexMap = "0123456789abcdef".toCharArray();
+        StringBuilder result = new StringBuilder();
+
+        // process at most 8 times (32 bits / 4 bits)
+        while (num != 0) {
+            int digit = num & 15;   // get last 4 bits
+            result.append(hexMap[digit]);
+            num >>>= 4;             // unsigned right shift
+        }
+
+        return result.reverse().toString();
+    }
+
+}
