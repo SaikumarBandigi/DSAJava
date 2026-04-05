6a663012eec8fc7171bb14a647dcfb98e3f96abb
Saikumar Bandigi <88974882+SaikumarBandigi@users.noreply.github.com>
2026-04-05 17:33:35 +0530

    Implement toLowerCase method in Solution class

---
+++ src/leetcode/stringconcept/ToLowerCase.java
@@ -0,0 +1,21 @@
+class Solution {
+
+    public String toLowerCase(String s) {
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < s.length(); i++) {
+            if (Character.isUpperCase(s.charAt(i))) {
+                sb.append(toLowerCase(s.charAt(i)));
+            } else {
+                sb.append(s.charAt(i));
+            }
+        }
+        return sb.toString();
+    }
+
+    char toLowerCase(char ch) {
+        return Character.toLowerCase(ch);
+    }
+
+}
