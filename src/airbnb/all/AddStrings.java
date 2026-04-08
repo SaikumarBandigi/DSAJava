1e4f57e5415267be0d1c5f79637c0f7b436a3bf3
Saikumar Bandigi <bandigisaikumar@gmail.com>
2026-04-08 21:11:54 +0530

    added AddStrings File

---
+++ src/airbnb/all/AddStrings.java
@@ -0,0 +1,37 @@
+package airbnb.all;
+
+public class AddStrings {
+
+    public static void main(String[] args) {
+
+        String num1 = "12", num2 = "12";
+        System.out.println(new AddStrings().addStrings(num1, num2));
+
+    }
+
+    public String addStrings(String num1, String num2) {
+
+        int i = num1.length() - 1;
+        int j = num2.length() - 1;
+        int carry = 0;
+
+        StringBuilder result = new StringBuilder();
+
+        while (i >= 0 || j >= 0 || carry > 0) {
+
+            int digit1 = (i >= 0) ? num1.charAt(i) - '0' : 0;
+            int digit2 = (j >= 0) ? num2.charAt(j) - '0' : 0;
+
+            int sum = digit1 + digit2 + carry;
+
+            result.append(sum % 10);   // store last digit
+            carry = sum / 10;          // update carry
+
+            i--;
+            j--;
+        }
+
+        return result.reverse().toString();
+    }
+
+}
