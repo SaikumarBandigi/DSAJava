87cf4a8ecb9a823858e273e181e74955aceddf57
Saikumar Bandigi <bandigisaikumar@gmail.com>
2026-05-07 11:41:05 +0530

    added UglyNumber File

--- src/leetcode/MathTut/CanMakeArithmeticProgressionFromSequence.java
+++ src/leetcode/MathTut/CanMakeArithmeticProgressionFromSequence.java
@@ -6,20 +6,21 @@ public class CanMakeArithmeticProgressionFromSequence {
 
     public static void main(String[] args) {
 
-        int[] arr = {3, 5, 1};
+        int[] arr = {1, 2, 4};
         System.out.println(new CanMakeArithmeticProgressionFromSequence().canMakeArithmeticProgression(arr));
     }
 
     public boolean canMakeArithmeticProgression(int[] arr) {
+        Arrays.sort(arr);  // 1 3 5
 
-        Arrays.sort(arr);
+        int d = arr[1] - arr[0];  // 1
 
-        int d = arr[0] - arr[1];
-        for (int i = 0; i < arr.length - 1; i++) {
-            if (arr[i] - arr[i + 1] != d) {
+        for (int i = 1; i < arr.length - 1; i++) {
+            if (arr[i + 1] - arr[i] != d) {
                 return false;
             }
         }
+
         return true;
     }
 
---
+++ src/leetcode/quest/UglyNumber.java
@@ -0,0 +1,24 @@
+package leetcode.quest;
+
+public class UglyNumber {
+
+    public static void main(String[] args) {
+
+        int n = 16;
+        System.out.println(new UglyNumber().isUgly(n));
+    }
+
+    public boolean isUgly(int n) {
+        if (n <= 0) return false;
+
+        int[] factors = {2, 3, 5};
+
+        for (int f : factors) {
+            while (n % f == 0) {
+                n /= f;
+            }
+        }
+        return n==1;
+    }
+
+}
