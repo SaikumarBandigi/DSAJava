c5ffb69a62a314746ca7b8127be9ecc691ebddae
Saikumar Bandigi <bandigisaikumar@gmail.com>
2026-05-07 10:53:40 +0530

    added Practise File

--- src/apple/Practise.java
+++ src/apple/Practise.java
@@ -3,31 +3,28 @@ package apple;
 import java.util.HashSet;
 import java.util.Set;
 
+public class Practise {
 
-// Sliding window + HashSet
-
-public class LongestSubstringWithoutRepeatingCharacters {
     public static void main(String[] args) {
 
-        String s = "aa";
-        System.out.println(new LongestSubstringWithoutRepeatingCharacters().lengthOfLongestSubstring(s));
+        String s = "aabc";
+        System.out.println(new Practise().get(s));
 
     }
 
-    public int lengthOfLongestSubstring(String s) {
+    int get(String s) {
+
         int left = 0;
         int maxLen = 0;
-
 
         Set<Character> set = new HashSet<>();
 
         for (int right = 0; right < s.length(); right++) {
             while (set.contains(s.charAt(right))) {
-                set.remove(s.charAt(left));
+                set.remove(s.charAt(right));
                 left++;
             }
             set.add(s.charAt(right));
-
             maxLen = Math.max(maxLen, right - left + 1);
         }
         return maxLen;
