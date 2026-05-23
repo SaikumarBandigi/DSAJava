726ba2744d2c5120bd526a7f7435b2db72f8221b
Saikumar Bandigi <bandigisaikumar@gmail.com>
2026-05-23 21:17:17 +0530

    updated numberOfStepsOptimized

--- src/leetcode/bitmanipulationtut/NumberofStepstoReduceaNumbertoZero.java
+++ src/leetcode/bitmanipulationtut/NumberofStepstoReduceaNumbertoZero.java
@@ -5,6 +5,7 @@ public class NumberofStepstoReduceaNumbertoZero {
     public static void main(String[] args) {
         int num = 14;
         System.out.println(new NumberofStepstoReduceaNumbertoZero().numberOfSteps(num));
+        System.out.println(new NumberofStepstoReduceaNumbertoZero().numberOfStepsOptimized(num));
     }
 
     public int numberOfSteps(int num) {
@@ -23,7 +24,22 @@ public class NumberofStepstoReduceaNumbertoZero {
     }
 
     boolean isEven(int num) {
-        return (num % 2) == 0;
+        return (num & 1) == 0;
+    }
+
+    public int numberOfStepsOptimized(int num) {
+
+        int steps = 0;
+
+        while (num != 0) {
+            if (isEven(num)) {
+                num = num >> 1;
+            } else {
+                num--;
+            }
+            steps++;
+        }
+        return steps;
     }
 
 }
