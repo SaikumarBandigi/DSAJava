f76599a22235090c50e424d614a5b2b283ce1ecd
Saikumar Bandigi <bandigisaikumar@gmail.com>
2026-05-31 08:07:52 +0530

    added DigitFrequencyScore File

---
+++ src/leetcodecontest/weekly/five04/DigitFrequencyScore.java
@@ -0,0 +1,36 @@
+package leetcodecontest.weekly.five04;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class DigitFrequencyScore {
+
+    public static void main(String[] args) {
+        int n = 122;
+        System.out.println(new DigitFrequencyScore().digitFrequencyScore(n));
+    }
+
+    public int digitFrequencyScore(int n) {
+
+        Map<Character, Integer> map = new HashMap<>();
+
+        String s = String.valueOf(n);
+
+        for (int i = 0; i < s.length(); i++) {
+            map.put(s.charAt(i), map.getOrDefault(s.charAt(i), 0) + 1);
+        }
+
+        int score = 0;
+
+        for (Map.Entry<Character, Integer> entry : map.entrySet()) {
+            char key = entry.getKey();
+            Integer value = entry.getValue();
+
+            score += (key - '0') * value;
+
+        }
+
+        return score;
+    }
+
+}
