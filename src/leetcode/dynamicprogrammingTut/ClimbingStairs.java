af0a3d2c84dc88e9d420271507459124a524012c
Saikumar Bandigi <bandigisaikumar@gmail.com>
2026-06-14 12:13:31 +0530

    added ClimbingStairs File

---
+++ src/leetcode/dynamicprogrammingTut/ClimbingStairs.java
@@ -0,0 +1,30 @@
+package leetcode.dynamicprogrammingTut;
+
+public class ClimbingStairs {
+
+    public static void main(String[] args) {
+
+        int n = 4;
+        System.out.println(new ClimbingStairs().climbStairs(n));
+
+    }
+
+    public int climbStairs(int n) {
+
+        if (n <= 2) {
+            return n;
+        }
+
+        int[] dp = new int[n + 1];
+
+        dp[1] = 1;
+        dp[2] = 2;
+
+        for (int i = 3; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+
+        return dp[n];
+    }
+
+}
---
+++ src/leetcodecontest/weekly/five06/FrequencyBalanceSubarray.java
@@ -0,0 +1,14 @@
+package leetcodecontest.weekly.five06;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class FrequencyBalanceSubarray {
+
+    public static void main(String[] args) {
+
+    }
+
+
+
+}
