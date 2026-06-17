222a2b0f3ba2c39fb0444876c9e81b3cdd58b791
Saikumar Bandigi <bandigisaikumar@gmail.com>
2026-06-17 17:50:23 +0530

    added SubstringsofSizeThreewithDistinctCharacters File

---
+++ src/leetcode/slidingwindowTut/SubstringsofSizeThreewithDistinctCharacters.java
@@ -0,0 +1,50 @@
+package leetcode.slidingwindowTut;
+
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class SubstringsofSizeThreewithDistinctCharacters {
+
+    public static void main(String[] args) {
+
+        String s = "xyzzaz";
+        System.out.println(new SubstringsofSizeThreewithDistinctCharacters().countGoodSubstrings(s));
+
+    }
+
+    public int countGoodSubstrings(String s) {
+        if (s.length() < 3) return 0;
+        String[] arr = new String[s.length() - 2];
+
+        for (int i = 0; i < s.length() - 2; i++) {
+            StringBuilder substring = new StringBuilder();
+            for (int j = i; j < i + 3 && j < s.length(); j++) {
+                substring.append(s.charAt(j));
+            }
+            arr[i] = substring.toString();
+        }
+
+        int count = 0;
+
+        for (String string : arr) {
+            if (isStringGood(string)) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+    boolean isStringGood(String s) {
+        Set<Character> set = new HashSet<>();
+
+        for (char ch : s.toCharArray()) {
+            if (!set.add(ch)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+}
---
+++ src/leetcode/stringconcept/ProcessStringwithSpecialOperationsII.java
@@ -0,0 +1,12 @@
+package leetcode.stringconcept;
+
+public class ProcessStringwithSpecialOperationsII {
+
+    public static void main(String[] args) {
+
+        String s = "a#b%*";
+        long k = 1;
+
+
+    }
+}
