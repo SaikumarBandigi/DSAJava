d8a526df7ebbeabd6fceef83373b590d963975a8
Saikumar Bandigi <bandigisaikumar@gmail.com>
2026-06-22 09:11:37 +0530

    added sumDivisibleByKUsingHashMap Method in SumofElementsWithFrequencyDivisiblebyK File

--- src/leetcode/HashMapTut/SumofElementsWithFrequencyDivisiblebyK.java
+++ src/leetcode/HashMapTut/SumofElementsWithFrequencyDivisiblebyK.java
@@ -1,5 +1,8 @@
 package leetcode.HashMapTut;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class SumofElementsWithFrequencyDivisiblebyK {
 
     public static void main(String[] args) {
@@ -8,6 +11,7 @@ public class SumofElementsWithFrequencyDivisiblebyK {
         int k = 3;
 
         System.out.println(new SumofElementsWithFrequencyDivisiblebyK().sumDivisibleByKOptimal(nums, k));
+        System.out.println(new SumofElementsWithFrequencyDivisiblebyK().sumDivisibleByKUsingHashMap(nums, k));
 
     }
 
@@ -27,6 +31,25 @@ public class SumofElementsWithFrequencyDivisiblebyK {
             }
         }
 
+        return sum;
+    }
+
+    private int sumDivisibleByKUsingHashMap(int[] nums, int k) {
+
+        // 4 -> 3
+        // 1 -> 1
+
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+
+        int sum = 0;
+        for (int num : nums) {
+            if (map.get(num) % k == 0) {
+                sum += num;
+            }
+        }
         return sum;
     }
 
