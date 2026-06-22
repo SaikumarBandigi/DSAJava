6cd1e8339b3faa6cdaf2f6fcfb1ee31fafd2f9f4
Saikumar Bandigi <bandigisaikumar@gmail.com>
2026-06-22 19:23:10 +0530

    added CountingBits File

---
+++ src/neetcode/blind75/CountingBits.java
@@ -0,0 +1,33 @@
+package neetcode.blind75;
+
+import java.util.Arrays;
+
+public class CountingBits {
+
+    public static void main(String[] args) {
+        int n = 4;
+        System.out.println(Arrays.toString(new CountingBits().countBits(n)));
+    }
+
+    public int[] countBits(int n) {
+        int[] arr = new int[n + 1];
+
+        for (int i = 0; i <= n; i++) {
+            arr[i] = getNumberOfBitsInASingleNumber(i);
+        }
+        return arr;
+    }
+
+    int getNumberOfBitsInASingleNumber(int num) {
+        String binaryString = Integer.toBinaryString(num);
+
+        int count = 0;
+        for (int i = 0; i < binaryString.length(); i++) {
+            if (binaryString.charAt(i) == '1') {
+                count++;
+            }
+        }
+        return count;
+    }
+
+}
